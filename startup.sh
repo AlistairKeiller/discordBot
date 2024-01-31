ab69690c95781179ae842473f0922b9a70e7f596
Alistair Keiller <alistair@keiller.net>
2024-01-30 17:17:33 -0800

    install git and cargo

--- startup.sh
+++ startup.sh
@@ -1,3 +1,5 @@
+sudo dnf install git -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 cd ~
 git clone https://github.com/AlistairKeiller/discordBot
